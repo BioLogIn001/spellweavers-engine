8dd5733b152bb62c672bdfb26529fc776b1da2a5
BioLogIn001 <biologin001@gmail.com>
2023-03-14 18:32:40 +0200

    Separated validator from parser in WarlockOrders

--- ruleset_warlocks/class_warlocks_orders.py
+++ ruleset_warlocks/class_warlocks_orders.py
@@ -88,9 +88,17 @@ class WarlocksOrders:
         valid_participant_ids = match_data.get_ids_participants_active()
 
         data = self.load_orders_from_file()
-        self.validate_orders(data, match_data.match_id, match_data.current_turn,
-                             match_data.hand_id_offset, valid_participant_ids,
-                             match_spellbook.valid_gestures, match_spellbook.valid_spell_ids)
+        for key in data:
+            validation_error_codes = self.validate_json_order(data[key], 
+                                                                match_data.match_id, 
+                                                                match_data.current_turn, 
+                                                                valid_participant_ids)
+            if not validation_error_codes:
+                new_order = self.parse_json_order(data[key], 
+                                                    match_data.hand_id_offset,
+                                                    match_spellbook.valid_gestures, 
+                                                    match_spellbook.valid_spell_ids)
+                self.orders.append(new_order)
 
     def check_missing_orders(self, match_data):
         """Check for missing orders for the turn using submitted active participants list.
@@ -132,122 +140,124 @@ class WarlocksOrders:
                 return order
         return None
 
-    def validate_orders(self, data, match_id, turn_num, hand_offset,
-                        valid_participant_ids, valid_gestures, valid_spell_ids):
+    def validate_json_order(self, data, match_id, turn_num, valid_participant_ids):
         """Validate incoming orders.
         
         Arguments:
             data (string): raw JSON data
             match_id (int): match ID
             turn_num (int): turn number
-            hand_offset (int): offset to calculate hand IDs (set to 10 for Warlocks)
             valid_participant_ids (list): IDs of participants that are expected to act this turn
+        """
+
+        validation_error_codes = []
+
+        if ('matchID' not in data) or (int(data['matchID']) != match_id):
+            # missing or invalid matchID
+            validation_error_codes.append(1)
+        if ('turnNum' not in data) or (int(data['turnNum']) != turn_num):
+            # missing or invalid turnNum
+            validation_error_codes.append(2)
+        if ('participantID' not in data) or (int(data['participantID']) not in valid_participant_ids):
+            # missing or invalid participantID
+            validation_error_codes.append(3)
+
+        return validation_error_codes
+
+    def parse_json_order(self, data, hand_offset, valid_gestures, valid_spell_ids):
+        """Parse JSON order.
+        
+        Arguments:
+            data (string): raw JSON data
+            hand_offset (int): offset to calculate hand IDs (set to 10 for Warlocks)
             valid_gestures (list): gestures (str(1)) that are valid for selected SpellBook
             valid_spell_ids (list): spell IDs (integer) that are valid for selected SpellBook
         """
 
-        for o in data:
+        default_gesture = '-'
+        default_spell_id = -1
+        default_target_id = -1
 
-            # Determine match ID, turn number, and participant ID from Orders
-            # and check them against current match - turn - participants.
-            if ('matchID' not in data[o]):
-                continue
-            else:
-                match_id_t = int(data[o]['matchID'])
-            if ('turnNum' not in data[o]):
-                continue
-            else:
-                turn_num_t = int(data[o]['turnNum'])
-            if ('participantID' not in data[o]):
-                continue
-            else:
-                participant_id_t = int(data[o]['participantID'])
+        # create order instance
+        new_order = WarlocksOrder()
+        new_order.match_id = int(data['matchID'])
+        new_order.turn_num = int(data['turnNum'])
+        new_order.participant_id = int(data['participantID'])
 
-            if ((match_id_t != match_id) or (turn_num_t != turn_num)
-                    or (participant_id_t not in valid_participant_ids)):
-                continue
+        # Get and validate LH and RH gestures
+        if ('gestureLH' not in data) or (data['gestureLH'][0] not in valid_gestures):
+            new_order.gesture_lh = default_gesture
+        else:
+            new_order.gesture_lh = data['gestureLH'][0]
+        if ('gestureRH' not in data) or (data['gestureRH'][0] not in valid_gestures):
+            new_order.gesture_rh = default_gesture
+        else:
+            new_order.gesture_rh = data['gestureRH'][0]
 
-            # If we expect an order from this participant on this turn of this match
-            # create order instance
-            new_order = WarlocksOrder()
-            new_order.match_id = match_id_t
-            new_order.turn_num = turn_num_t
-            new_order.participant_id = participant_id_t
+        # Get and validate requested LH and RH spell IDs
+        if ('orderSpellLH' not in data) or (int(data['orderSpellLH']) not in valid_spell_ids):
+            new_order.order_spell_lh = default_spell_id
+        else:
+            new_order.order_spell_lh = int(data['orderSpellLH'])
+        if ('orderSpellRH' not in data) or (int(data['orderSpellRH']) not in valid_spell_ids):
+            new_order.order_spell_rh = default_spell_id
+        else:
+            new_order.order_spell_rh = int(data['orderSpellRH'])
 
-            # Get and validate LH and RH gestures
-            if ('gestureLH' not in data[o]) or (data[o]['gestureLH'][0] not in valid_gestures):
-                new_order.gesture_lh = '-'
-            else:
-                new_order.gesture_lh = data[o]['gestureLH'][0]
-            if ('gestureRH' not in data[o]) or (data[o]['gestureRH'][0] not in valid_gestures):
-                new_order.gesture_rh = '-'
-            else:
-                new_order.gesture_rh = data[o]['gestureRH'][0]
+        # Get and validate requested LH and RH targets
+        if ('orderTargetLH' not in data):
+            new_order.order_target_lh = default_target_id
+        else:
+            new_order.order_target_lh = int(data['orderTargetLH'])
+        if ('orderTargetRH' not in data):
+            new_order.order_target_rh = default_target_id
+        else:
+            new_order.order_target_rh = int(data['orderTargetRH'])
 
-            # Get and validate requested LH and RH spell IDs
-            if ('orderSpellLH' not in data[o]) or (int(data[o]['orderSpellLH']) not in valid_spell_ids):
-                new_order.order_spell_lh = -1
-            else:
-                new_order.order_spell_lh = int(data[o]['orderSpellLH'])
-            if ('orderSpellRH' not in data[o]) or (int(data[o]['orderSpellRH']) not in valid_spell_ids):
-                new_order.order_spell_rh = -1
-            else:
-                new_order.order_spell_rh = int(data[o]['orderSpellRH'])
+        # Get and validate special action - delay spell
+        if ('delaySpell' not in data):
+            new_order.delay_spell = 0
+        else:
+            new_order.delay_spell = int(data['delaySpell'])
 
-            # Get and validate requested LH and RH targets
-            if ('orderTargetLH' not in data[o]):
-                new_order.order_target_lh = -1
-            else:
-                new_order.order_target_lh = int(data[o]['orderTargetLH'])
-            if ('orderTargetRH' not in data[o]):
-                new_order.order_target_rh = -1
-            else:
-                new_order.order_target_rh = int(data[o]['orderTargetRH'])
+        # Get and validate special action - fire delayed spell
+        if ('castDelayedSpell' not in data) or (int(data['castDelayedSpell']) not in [1]):
+            new_order.cast_delayed_spell = 0
+        else:
+            new_order.cast_delayed_spell = int(data['castDelayedSpell'])
 
-            # Get and validate special action - delay spell
-            if ('delaySpell' not in data[o]):
-                new_order.delay_spell = 0
-            else:
-                new_order.delay_spell = int(data[o]['delaySpell'])
+        # Get and validate special action - make spell permanent
+        if ('makeSpellPermanent' not in data):
+            new_order.make_spell_permanent = 0
+        else:
+            new_order.make_spell_permanent = int(
+                data['makeSpellPermanent'])
 
-            # Get and validate special action - fire delayed spell
-            if ('castDelayedSpell' not in data[o]) or (int(data[o]['castDelayedSpell']) not in [1]):
-                new_order.cast_delayed_spell = 0
-            else:
-                new_order.cast_delayed_spell = int(data[o]['castDelayedSpell'])
+        # Get and validate special action - commit suicide (if under a permanent mindspell)
+        if ('commitSuicide' not in data) or (int(data['commitSuicide']) not in [1]):
+            new_order.commit_suicide = 0
+        else:
+            new_order.commit_suicide = int(data['commitSuicide'])
 
-            # Get and validate special action - make spell permanent
-            if ('makeSpellPermanent' not in data[o]):
-                new_order.make_spell_permanent = 0
-            else:
-                new_order.make_spell_permanent = int(
-                    data[o]['makeSpellPermanent'])
+        # Get and validate orders for paralyze
+        if ('paralyzeOrders' in data):
+            for i in data['paralyzeOrders']:
+                if data['paralyzeOrders'][i] is not None:
+                    new_order.paralyze_orders[int(
+                        i) // hand_offset] = int(i)
 
-            # Get and validate special action - commit suicide (if under a permanent mindspell)
-            if ('commitSuicide' not in data[o]) or (int(data[o]['commitSuicide']) not in [1]):
-                new_order.commit_suicide = 0
-            else:
-                new_order.commit_suicide = int(data[o]['commitSuicide'])
+        # Get and validate orders for charm person
+        if ('charmOrders' in data):
+            for i in data['charmOrders']:
+                if data['charmOrders'][i] is not None:
+                    new_order.charm_orders[int(
+                        i) // hand_offset] = (int(i), data['charmOrders'][i])
 
-            # Get and validate orders for paralyze
-            if ('paralyzeOrders' in data[o]):
-                for i in data[o]['paralyzeOrders']:
-                    if data[o]['paralyzeOrders'][i] is not None:
-                        new_order.paralyze_orders[int(
-                            i) // hand_offset] = int(i)
+        # Get and validate attack orders
+        if ('attackOrders' in data):
+            for i in data['attackOrders']:
+                if data['attackOrders'][i] is not None:
+                    new_order.attack_orders[int(i)] = int(
+                        data['attackOrders'][i])
 
-            # Get and validate orders for charm person
-            if ('charmOrders' in data[o]):
-                for i in data[o]['charmOrders']:
-                    if data[o]['charmOrders'][i] is not None:
-                        new_order.charm_orders[int(
-                            i) // hand_offset] = (int(i), data[o]['charmOrders'][i])
-
-            # Get and validate attack orders
-            if ('attackOrders' in data[o]):
-                for i in data[o]['attackOrders']:
-                    if data[o]['attackOrders'][i] is not None:
-                        new_order.attack_orders[int(i)] = int(
-                            data[o]['attackOrders'][i])
-
-            self.orders.append(new_order)
+        return new_order
