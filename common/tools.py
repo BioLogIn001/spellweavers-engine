66f86e15b55c3b459e1df6bc4feeea6e9a8ce381
Igor <biologin001@gmail.com>
2023-12-09 17:03:23 +0200

    Update tools.py

--- common/tools.py
+++ common/tools.py
@@ -1,9 +1,11 @@
 def import_name(modulename, name):
     """Import a named object from a module in the context of this function.
+
     Source: https://www.oreilly.com/library/view/python-cookbook/0596001673/ch15s04.html
     """
     try:
         module = __import__(modulename, globals(), locals(), [name])
     except ImportError:
-        return None
+        raise Exception("Module " + modulename + " not found")
+        # return None
     return vars(module)[name]
